38658226ab6bc159eea41e1ff07f698b769e97ac
Tarun <trnpandey6@gmail.com>
2025-05-17 02:41:05 +0530

    Update UserRepository.java

--- backend/src/main/java/com/app/repository/UserRepository.java
+++ backend/src/main/java/com/app/repository/UserRepository.java
@@ -1,1 +1,6 @@
+package com.app.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.app.model.UserData;
+
 public interface UserRepository extends MongoRepository<UserData, String> {}
