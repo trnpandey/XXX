5afbf2766e6d052dc1c3e0027de23c0ccd840e04
Tarun <trnpandey6@gmail.com>
2025-05-17 02:20:54 +0530

    Create Error.tsx

---
+++ frontend/src/pages/Error.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Error() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
