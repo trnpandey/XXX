4f8d244d1b4341aa36503f90dbe6c62a4cdb8719
Tarun <trnpandey6@gmail.com>
2025-05-17 02:38:23 +0530

    Update FeedbackController.java

--- backend/src/main/java/com/app/controller/FeedbackController.java
+++ backend/src/main/java/com/app/controller/FeedbackController.java
@@ -1,6 +1,20 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+import com.app.repository.SessionRepository;
+import com.app.repository.UserRepository;
+
 @RestController
 @RequestMapping("/api/feedback")
 public class FeedbackController {
+
     @Autowired private SessionRepository sessionRepo;
     @Autowired private UserRepository userRepo;
 
