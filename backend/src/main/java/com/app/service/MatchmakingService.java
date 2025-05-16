58875e01ff8c8866cb1c7e1c5aed3212b624d2da
Tarun <trnpandey6@gmail.com>
2025-05-17 02:42:27 +0530

    Update MatchmakingService.java

--- backend/src/main/java/com/app/service/MatchmakingService.java
+++ backend/src/main/java/com/app/service/MatchmakingService.java
@@ -1,5 +1,23 @@
+package com.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RQueue;
+import org.redisson.api.RedissonClient;
+
+import com.app.model.Session;
+import com.app.repository.SessionRepository;
+
 @Service
 public class MatchmakingService {
+
     @Autowired private RedissonClient redissonClient;
     @Autowired private SessionRepository sessionRepo;
     @Autowired private SimpMessagingTemplate messagingTemplate;
