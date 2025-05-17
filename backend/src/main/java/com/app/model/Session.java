50e6aae1be3e4585194830159445082c1d9bd906
Tarun <trnpandey6@gmail.com>
2025-05-17 22:32:15 +0530

    Update Session.java

--- backend/src/main/java/com/app/model/Session.java
+++ backend/src/main/java/com/app/model/Session.java
@@ -1,18 +1,15 @@
 package com.app.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 
-@Document(collection = "sessions")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Session {
-    @Id
     private String sessionId;
     private String venterId;
     private String listenerId;
