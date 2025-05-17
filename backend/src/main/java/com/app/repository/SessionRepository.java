2c11bed44e7f9a0b698d72652a3faab72d6c3cde
Tarun <trnpandey6@gmail.com>
2025-05-17 22:36:26 +0530

    Delete backend/src/main/java/com/app/repository directory

--- backend/src/main/java/com/app/repository/SessionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.app.model.Session;
-
-public interface SessionRepository extends MongoRepository<Session, String> {}
--- backend/src/main/java/com/app/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.app.model.UserData;
-
-public interface UserRepository extends MongoRepository<UserData, String> {}
