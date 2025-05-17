fa0253523629880eb6cdeb9415854464b35cdfed
Tarun <trnpandey6@gmail.com>
2025-05-17 22:34:49 +0530

    Update MatchmakingService.java

--- backend/src/main/java/com/app/service/MatchmakingService.java
+++ backend/src/main/java/com/app/service/MatchmakingService.java
@@ -3,13 +3,9 @@ package com.app.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import com.app.repository.SessionRepository;
 
 @Service
 public class MatchmakingService {
-
-    @Autowired
-    private SessionRepository sessionRepo;
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
