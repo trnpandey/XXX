3f62225af0f2269d9ae400d199e21cb73b1abd12
Tarun <trnpandey6@gmail.com>
2025-05-17 22:32:02 +0530

    Update UserData.java

--- backend/src/main/java/com/app/model/UserData.java
+++ backend/src/main/java/com/app/model/UserData.java
@@ -1,14 +1,10 @@
 package com.app.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "userData")
 public class UserData {
-    @Id
-    private String userId; // Google email
+    private String userId; 
     private double venterRating;
     private double listenerRating;
     private int karmaPoints;
-    // getters/setters/constructors omitted for brevity
 }
